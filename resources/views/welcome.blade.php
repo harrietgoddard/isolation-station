1aa5b5d4c289b8c4bc5f75d8595fbf0bd5c235f2
Harriet Goddard <goddardhe@gmail.com>
2020-05-18 22:14:11 +0100

    lists added to homepage

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,44 @@
 
 @section("content")
 
-    <h1>Isolation Station</h1>
+    <h1 class="text-center display-3">Isolation Station</h1>
+    <h4 class="text-center font-italic mb-5">The self-isolation journalling app</h4>
+
+    <div class="d-flex justify-content-between">
+
+        <div class="card" style="width: 18rem;">
+            <div class="card-header">
+                What are you grateful for today?
+            </div>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">Cras justo odio</li>
+                <li class="list-group-item">Dapibus ac facilisis in</li>
+                <li class="list-group-item">Vestibulum at eros</li>
+            </ul>
+        </div>
+
+        <div class="card" style="width: 18rem;">
+            <div class="card-header">
+                What have you achieved today?
+            </div>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">Cras justo odio</li>
+                <li class="list-group-item">Dapibus ac facilisis in</li>
+                <li class="list-group-item">Vestibulum at eros</li>
+            </ul>
+        </div>
+
+        <div class="card" style="width: 18rem;">
+            <div class="card-header">
+                Any resolutions for tomorrow?
+            </div>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item">Cras justo odio</li>
+                <li class="list-group-item">Dapibus ac facilisis in</li>
+                <li class="list-group-item">Vestibulum at eros</li>
+            </ul>
+        </div>
+
+    </div>
 
 @endsection

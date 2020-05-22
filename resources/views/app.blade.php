03188ebf8e69ee2d99f0841b0fb7335fd8e051c5
Harriet Goddard <goddardhe@gmail.com>
2020-05-22 14:02:12 +0100

    styles move to separate blade file

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -14,48 +14,7 @@
         <link href="https://fonts.googleapis.com/css2?family=Pacifico&family=Quicksand:wght@400;500&display=swap" rel="stylesheet">
         <meta name="theme-color" content="#fafafa">
 
-        <style>
-            body {
-              font-family: 'Quicksand';
-              background: aliceblue;
-            }
-            h1, .sub-head {
-              font-family: 'Pacifico';
-              color: darkslategray;
-              font-weight: 500;
-            }
-            .strapline {
-              font-size: 1.2rem;
-              color: dimgray;
-            }
-            .card-header {
-              font-size: 1.5rem;
-            }
-            .card {
-              border-radius: 10px;
-              box-shadow: 0 8px 16px 0 rgba(0,0,0,0.7);
-            }
-            .hero-card {
-              width: 21rem;
-            }
-            .card-header:first-child {
-              border-radius: 10px;
-            }
-            a, a:hover {
-              text-decoration: none;
-              color: inherit;
-            }
-            .nav-link {
-              font-size: 1.2rem;
-              color: darkslategray;
-              border: 1px solid dimgray;
-              border-radius: 5px;
-            }
-            .btn {
-              background: #7AE7C7;
-              color: black;
-            }
-        </style>
+        @include("styles/styles")
 
     </head>
 
